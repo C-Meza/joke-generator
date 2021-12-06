de432d103983690aa44ea12821c386206d0e1686
Cesar Meza <cesar.me8957@vusd.us>
2021-12-06 13:57:56 -0800

    better

---
+++ app/src/main/java/com/example/jokegenerator/JokeDB.java
@@ -0,0 +1,96 @@
+package com.example.jokegenerator;
+
+public class JokeDB {
+
+
+    public static final String[] dadJokes = {
+            "I'm afraid for the calendar. Its days are numbered.", "My wife said I should do lunges to stay in shape. \nThat would be a big step forward.",
+            "Why do fathers take an extra pair of socks when they go golfing? \nIn case they get a hole in one!",
+            "Singing in the shower is fun until you get soap in your mouth. " +
+                    "\nThen it's a soap opera.",
+            "What do a tick and the Eiffel Tower have in common?" + "\nThey're both Paris sites.",
+            "What do you call a fish wearing a bowtie?" + "\nSofishticated.",
+            "How do you follow Will Smith in the snow?" + "\nYou follow the fresh prints.",
+            "If April showers bring May flowers, what do May flowers bring?" + "\nPilgrims.",
+            "I thought the dryer was shrinking my clothes. \nTurns out it was the refrigerator all along.", "What do you call a factory that makes okay products?" + "\nA satisfactory.",
+            "Dear Math, grow up and solve your own problems.",
+            "What did the janitor say when he jumped out of the closet?" + "\nSupplies!",
+            "Have you heard about the chocolate record player? \nIt sounds pretty sweet.",
+            "What did the ocean say to the beach?" + "\nNothing, it just waved.",
+            "Why do seagulls fly over the ocean?" + "\nBecause if they flew over the bay, we'd call them bagels."
+
+    };
+
+    public  static final String[] antiJokes = {
+
+      /* What do you call a joke that isn’t funny?
+A sentence.
+2. What did one stranger say to the other?
+Nothing. They didn’t know each other.
+3. How do you confuse someone?
+Paint yourself green and throw forks at them.
+4. What did one Frenchman say to the other Frenchman?
+I don’t know; I don’t speak French.
+5. Why did the swan hiss?
+Biologically, it’s coded in their genes to do so when threatened.
+6. What do you call 100 lawyers at the bottom of the ocean?
+A horrible boating accident.
+7. Why did the teacher tell Jamie she was wearing too much makeup?
+Because she was wearing too much makeup.
+8. I told my friend she was drawing her eyebrows too high.
+She looked surprised.
+9. What do you call a pigeon that can’t find its way back home?
+A pigeon.
+10. What’s worse than finding a worm in your apple?
+Being robbed.
+11. Why did Benjamin get sick after eating too much ice cream?
+He was lactose intolerant.
+12. How is a laser beam similar to a goldfish?
+Neither one can whistle.
+13. I accused my husband of being too immature.
+Then he told me to get out of his fort.
+14. How do you get someone to stop swinging on the tire swing?
+Snip the rope.
+15. What did the man say when he lost his truck?
+“Where’s my truck?”*/
+    };
+
+
+    public  static final String[] oneLiners = {
+
+            /*
+            "Light travels faster than sound. This is why some people appear bright until they open their mouths.",
+            "Money talks. But all mine ever says is goodbye."
+
+"Life's like a bird. It's pretty cute until it poops on your head."
+
+"I'm skeptical of anyone who tells me they do yoga every day. That's a bit of a stretch."
+
+"A computer once beat me at chess. But it was no match for me at kickboxing."
+
+"I have a lot of growing up to do. I realized that the other day inside my fort."
+
+
+"My therapist says I have a preoccupation with vengeance. We'll see about that."
+
+"Money can't buy you happiness? Well, check this out, I bought myself a Happy Meal!"
+
+"The easiest time to add insult to injury is when you're signing somebody's cast."
+
+
+"Letting go of a loved one can be hard. But sometimes, it's the only way to survive a rock climbing catastrophe."
+
+"A positive attitude may not solve all your problems. But it will annoy enough people to make it worth the effort."
+
+"Always borrow money from a pessimist. He won't expect it back."
+
+"Build a man a fire, and he'll be warm for a day. Set a man on fire, and he'll be warm for the rest of his life."
+
+"I used to be indecisive. Now I'm not sure."
+
+"Women should not have children after 35. Really, 35 children are enough."            */
+    };
+
+  //TODO /*https://www.techiedelight.com/merge-multiple-arrays-java/ */ public static final List<String>
+
+}
